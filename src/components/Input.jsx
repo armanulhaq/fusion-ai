4f31b366eae9aa273d948aa84b07388a631ec8fb
Arman Ul Haq <72503393+armanulhaq@users.noreply.github.com>
2024-11-21 14:07:31 +0530

    add textarea for input and responsive

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,21 +1,41 @@
+import { useEffect, useRef } from "react";
 import SendRoundedIcon from "@mui/icons-material/SendRounded";
 import styles from "./input.module.css";
+
 const Input = ({ query, setQuery, onSubmit }) => {
+    const textareaRef = useRef(null);
+
+    const handleInputChange = (e) => {
+        setQuery(e.target.value);
+    };
+
+    useEffect(() => {
+        if (textareaRef.current) {
+            // This clears any previously set height and Prevents the height from "sticking" to a larger size when text is deleted.
+            textareaRef.current.style.height = "auto";
+            // Dynamically adjusts the height to match the current content and Ensures the <textarea> grows as new text is added.
+            textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+        }
+    }, [query]);
+
     return (
         <div className={styles.inputAndSubmit}>
-            <input
-                placeholder="Ask Fusion AI"
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-                type="text"
-                className={styles.input}
-            />
-            <SendRoundedIcon
-                fontSize="large"
-                cursor="pointer"
-                onSubmit={onSubmit}
-                className="send-icon"
-            />
+            <div className={styles.textareaContainer}>
+                <textarea
+                    ref={textareaRef}
+                    placeholder="Ask Fusion AI"
+                    value={query}
+                    onChange={handleInputChange}
+                    className={styles.input}
+                    rows={1} // Start with one row
+                    style={{ maxHeight: "200px" }} // Set your max height here
+                />
+                <SendRoundedIcon
+                    cursor="pointer"
+                    onClick={onSubmit} // Call onSubmit when clicked
+                    className={styles.sendIcon}
+                />
+            </div>
         </div>
     );
 };
